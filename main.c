c77de27851c429318f835bb7b0bc4c84c6a5bcb6
zue4331 <71508905+zue4331@users.noreply.github.com>
2020-12-18 00:38:08 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+char pos[5];	//座號
+int i;			//給迴圈用的
+int rpos[5];	//給隨機位置用的
+
+int randpos() {
+	pos[0] = 'a';
+	pos[1] = 'b';
+	pos[2] = 'c';
+	pos[3] = 'd';
+	pos[4] = 'e';	//初始化，確認沒有空字元
+	rpos[0] = 10; 
+	rpos[1] = 20; 
+	rpos[2] = 30; 
+	rpos[3] = 40; 
+	rpos[4] = 50;	//初始化，防止一開始判斷錯誤
+	
+	i = 0;			//開始迴圈前的初始化
+	while (1) {
+		rpos[i] = (rand() % 5);
+		if (rpos[i] == rpos[i - 1] || rpos[i] == rpos[i - 2] || rpos[i] == rpos[i - 3] || rpos[i] == rpos[i - 4]) continue;  //防止相同位置
+		pos[rpos[i]] = '1' + i;
+		i++;
+		if (i >= 5) break;	
+
+	}
+//////////////////////////////以下為測試有無正常分配座號////////////////////////////////////
+	i = 0;
+	while (1) {
+		printf("%c\n", pos[i]);
+		i++;
+		if (i >= 5) break;
+	}
+}
+
+int main() {
+	srand(time(NULL));	
+	randpos();
+	
+	return 0;
+}
